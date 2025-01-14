1d8fafc9a13f0c2007d1fa42015d8268db58604f
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2025-01-14 11:16:06 -0300

    chage theme config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,64 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "gruvbox",
+  theme = "kanagawa",
 
   hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+M.ui = {
+  cmp = {
+    lspkind_text = true,
+    style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+    format_colors = {
+      tailwind = true,
+    },
+  },
+
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "block",
+    order = nil,
+    modules = nil,
+  },
+}
+
+M.nvdash = {
+
+  load_on_startup = false,
+
+  header = {
+    "                            ",
+    "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+    "   ▄▀███▄     ▄██ █████▀    ",
+    "   ██▄▀███▄   ███           ",
+    "   ███  ▀███▄ ███           ",
+    "   ███    ▀██ ███           ",
+    "   ███      ▀ ███           ",
+    "   ▀██ █████▄▀█▀▄██████▄    ",
+    "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+    "                            ",
+    "     Powered By  eovim    ",
+    "                            ",
+  },
+
+  buttons = {
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    -- more... check nvconfig.lua file for full list of buttons
+  },
 }
 
 return M
