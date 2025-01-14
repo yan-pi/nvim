b611fb38353e38aaec86ef71b42925654dc4effa
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2025-01-14 10:19:52 -0300

    update packages and colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,13 +2,20 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  -- add vesper
+  -- { "datsfilipe/vesper.nvim" },
+
+  --  kanagawa
+  { "rebelot/kanagawa.nvim", lazy = true },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "catppuccin-frappe",
       -- colorscheme = "rose-pine-moon",
-      colorscheme = "gruvbox",
+      colorscheme = "kanagawa",
+      -- colorscheme = "vesper",
     },
   },
 }
