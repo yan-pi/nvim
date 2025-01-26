5b0dc7931c754a53413d2f9c7456cd6f24b61708
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2025-01-26 12:32:37 -0300

    update and set config for mason handle lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,20 @@
 return {
   {
     "williamboman/mason.nvim",
+    cmd = "Mason",
+    opts = {
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
         "lua-language-server",
@@ -9,8 +23,37 @@ return {
         "css-lsp",
         "typescript-language-server",
         "prettier",
+        "tailwindcss",
+        "tsserver",
+        "gopls",
       },
+      automatic_installation = true,
     },
   },
-  { "williamboman/mason-lspconfig.nvim" },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "eslint_d",
+        "gofmt",
+        "goimports",
+      },
+      automatic_installation = true,
+    },
+  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "lua-language-server",
+  --       "stylua",
+  --       "html-lsp",
+  --       "css-lsp",
+  --       "typescript-language-server",
+  --       "prettier",
+  --     },
+  --   },
+  -- },
+  -- { "williamboman/mason-lspconfig.nvim" },
 }
