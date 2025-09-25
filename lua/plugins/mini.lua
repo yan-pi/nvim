bd722b069278b234ddb3b1206024c02747daa761
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2025-09-25 00:12:10 -0300

    adjus mini.files config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,12 +11,60 @@ return {
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       -- require('mini.ai').setup { n_lines = 500 }
-      require('mini.files').setup()
-      require('mini.pick').setup()
-      require('mini.move').setup()
-      require('mini.files').setup()
+
+      -- File explorer with column view
+      require('mini.files').setup {
+        options = {
+          use_as_default_explorer = true, -- Keep alongside netrw/telescope
+        },
+        mappings = {
+          close = '<Esc>',       -- Escape to close (intuitive)
+          go_in = 'l',           -- l to enter/open (vim-like)
+          go_in_plus = 'L',      -- L for advanced entry
+          go_out = 'h',          -- h for parent directory (vim-like)
+          go_out_plus = 'H',     -- H for advanced parent
+          mark_goto = "'",       -- Bookmark navigation
+          mark_set = 'm',        -- Bookmark setting
+          reset = '<BS>',        -- Backspace reset
+          reveal_cwd = '@',      -- Reveal cwd
+          show_help = 'g?',      -- Help
+          synchronize = '=',     -- Sync (ESSENTIAL for saving changes)
+          trim_left = '<',       -- Trim left
+          trim_right = '>',      -- Trim right
+        },
+      }
+
+      -- Add buffer-local Enter key support for mini.files
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          -- Add Enter as intuitive alternative to 'l'
+          vim.keymap.set('n', '<CR>', 'l', {
+            buffer = buf_id,
+            remap = true,
+            desc = 'Enter directory/open file'
+          })
+        end,
+      })
+
+      -- Fuzzy picker (lightweight alternative to Telescope)
+      require('mini.pick').setup {
+        options = {
+          use_cache = true, -- Better performance
+        },
+      }
+
+      -- Move lines and blocks with Alt+hjkl
+      require('mini.move').setup() -- Uses default Alt+hjkl mappings
+
+      -- Start screen
       require('mini.starter').setup()
+
+      -- Navigate with ][ shortcuts (]b for next buffer, etc.)
       require('mini.bracketed').setup()
+
+      -- Indent scope visualization
       require('mini.indentscope').setup()
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
@@ -40,6 +88,47 @@ return {
       statusline.section_location = function()
         return '%2l:%-2v'
       end
+
+      -- === KEYMAPS ===
+
+      -- Mini.files - File Explorer
+      vim.keymap.set('n', '<leader>e', function()
+        local current_file = vim.api.nvim_buf_get_name(0)
+        -- Check if current buffer has a valid file path
+        if current_file == '' or current_file:match('^%w+://') then
+          -- Open at current working directory if no valid file or special buffer
+          MiniFiles.open()
+        else
+          -- Open at current file's location
+          MiniFiles.open(current_file)
+        end
+      end, { desc = 'File [E]xplorer (current file)' })
+
+      vim.keymap.set('n', '<leader>E', function()
+        MiniFiles.open()
+      end, { desc = 'File [E]xplorer (cwd)' })
+
+      -- Mini.pick - Fuzzy Picker
+      vim.keymap.set('n', '<leader>pf', function()
+        MiniPick.builtin.files()
+      end, { desc = '[P]ick [F]iles' })
+
+      vim.keymap.set('n', '<leader>pb', function()
+        MiniPick.builtin.buffers()
+      end, { desc = '[P]ick [B]uffers' })
+
+      vim.keymap.set('n', '<leader>ph', function()
+        MiniPick.builtin.help()
+      end, { desc = '[P]ick [H]elp' })
+
+      vim.keymap.set('n', '<leader>pg', function()
+        MiniPick.builtin.grep_live()
+      end, { desc = '[P]ick [G]rep Live' })
+
+      -- Quick file picker (common IDE pattern)
+      vim.keymap.set('n', '<C-p>', function()
+        MiniPick.builtin.files()
+      end, { desc = 'Pick Files (Ctrl+P)' })
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
