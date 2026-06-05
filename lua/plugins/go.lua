3a3afd6d47ee2d116e7acc755498cde0f4cd0726
yan <102800044+yan-pi@users.noreply.github.com>
2026-06-05 16:25:09 -0300

    feat(go): add gofumpt stricter formatter
    Register gofumpt in mason-tool-installer for stricter Go code
    formatting beyond goimports.

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -140,6 +140,7 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         'go-debug-adapter', -- Delve debugger adapter for DAP
+        'gofumpt', -- Stricter Go formatter
       })
     end,
   },
