1b713ec5da3e96932e571d388980c9df2235e77d
yan <102800044+yan-pi@users.noreply.github.com>
2026-06-05 17:03:48 -0300

    feat(keymaps): add move cursor harness keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -27,10 +27,10 @@ vim.keymap.set('n', '<leader>b', ':enew<CR>', { desc = 'Create new empty buffer'
 vim.keymap.set('t', '<C-n>', '<C-\\><C-n>', { desc = 'Enter normal mode in terminal' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
