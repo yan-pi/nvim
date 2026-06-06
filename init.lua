5813fd2fd5ce0e5effcf5e20afde369e2907b38f
yan <102800044+yan-pi@users.noreply.github.com>
2026-06-05 22:42:03 -0300

    chore(init): load core.git module

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 require 'core.options'
 require 'core.keymaps'
 require 'core.autocmds'
+require 'core.git'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
