5f27c6e305a46381200e3bcd7f0ecb8fbf98e6d2
yan <102800044+yan-pi@users.noreply.github.com>
2026-06-05 22:42:04 -0300

    feat(autocmds): add cursor restore and auto-reload on focus gain

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -11,3 +11,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- Restore cursor to last known position when reopening a file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore cursor to last known position',
+  group = vim.api.nvim_create_augroup('user-restore-cursor', { clear = true }),
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, { mark[1], mark[2] })
+      vim.cmd('normal! zv')
+    end
+  end,
+})
+
+-- Check for external file changes on focus gain or cursor hold
+vim.api.nvim_create_autocmd({ 'FocusGained', 'CursorHold' }, {
+  desc = 'Check for external file changes',
+  group = vim.api.nvim_create_augroup('user-auto-reload', { clear = true }),
+  callback = function()
+    if vim.fn.getcmdwintype() == '' then
+      vim.cmd('checktime')
+    end
+  end,
+})
